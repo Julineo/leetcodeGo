1fe13e635fb1356336f56c7b19750df0f60db9b4
Julineo <advoretskiy80@gmail.com>
2018-03-20 17:04:26 -0700

    worked

--- StringToIntegerAtoi/main.go
+++ StringToIntegerAtoi/main.go
@@ -2,12 +2,21 @@ package main
 
 import (
 	"fmt"
+	"unicode"
 )
 
 func main() {
-
+	fmt.Println(myAtoi("-345 54"))
 }
 
 func myAtoi(str string) int {
-
+	runes := []rune(str)
+	digits := []rune{}
+	for _, r := range(runes) {
+		if unicode.IsDigit(r) {
+			digits = append(digits, r)
+		}
+	fmt.Println(digits)
+	}
+return 0
 }
