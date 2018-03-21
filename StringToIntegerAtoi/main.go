572b9ca2d38fadaa4359f5336e7e649075b66393
Julineo <advoretskiy80@gmail.com>
2018-03-21 11:00:48 -0700

    solved

--- StringToIntegerAtoi/main.go
+++ StringToIntegerAtoi/main.go
@@ -3,20 +3,58 @@ package main
 import (
 	"fmt"
 	"unicode"
+	"math"
+	"strings"
 )
 
 func main() {
-	fmt.Println(myAtoi("-345 54"))
+	fmt.Println(myAtoi("-9223372036854775809"))
 }
 
 func myAtoi(str string) int {
+	str = strings.TrimSpace(str)
+	if len(str) == 0 {
+		return 0
+	}
+	minusOne := int64(1)
+	if rune(str[0]) == '-' { //check if negative
+		minusOne = -1
+	}
 	runes := []rune(str)
-	digits := []rune{}
+	digits := []int64{}
+	if runes[0] == '+' || runes[0] == '-' {
+		runes = runes[1:]
+	}
 	for _, r := range(runes) {
 		if unicode.IsDigit(r) {
-			digits = append(digits, r)
+			digits = append(digits, int64(r)-48)
+			continue
+		} else {
+			break
 		}
-	fmt.Println(digits)
+		if r == ' ' && len(digits) > 0 {
+			break 
+		}
+		if !unicode.IsDigit(r) && len(digits) > 0 {
+			break
+		}
 	}
-return 0
+	if len(digits) == 0 {
+		return 0
+	}
+	result := digits[0]
+	for i := 1; i < len(digits); i++ {
+		result = result * 10 + digits[i]
+		if result > math.MaxInt32 + 1 {
+			break
+		}
+	}
+	result = result * minusOne
+	if result < math.MinInt32 && result < 0 {
+		return math.MinInt32
+	}
+	if result > math.MaxInt32 && result > 0 {
+		return math.MaxInt32
+	}
+	return int(result)
 }
