67d47e1d00c6cf0a668a3892442ea23888c0f81c
Julineo <advoretskiy80@gmail.com>
2018-04-17 10:41:31 -0700

    solved

--- BestTimetoBuyII/main.go
+++ BestTimetoBuyII/main.go
@@ -3,24 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("%v\n", maxProfit([]int{1,2}))//1
-	fmt.Printf("%v\n", maxProfit([]int{2,1}))//0
+	fmt.Printf("%v\n", maxProfit([]int{})) //0
+	fmt.Printf("%v\n", maxProfit([]int{1, 2})) //1
+	fmt.Printf("%v\n", maxProfit([]int{2, 1})) //0
 }
 
 func maxProfit(prices []int) int {
-    i := 0
-    valley, peak, profit := 0, 0, 0
-    for i < len(prices) - 1 {
-        for i < len(prices) - 1 && prices[i] >= prices[i + 1] {
-            peak = prices[i]
-            i++
-        } 
-        for i < len(prices) - 1 && prices[i] <= prices[i + 1] {
-            valley = prices[i]
-            peak = prices[i+1]
-            i++
-        }
-        profit = profit + peak - valley
-    }
-    return profit
+	if len(prices) == 0 {
+		return 0
+	}
+	i := 0
+	valley, peak, profit := prices[0], prices[0], 0
+	for i < len(prices)-1 {
+		for i < len(prices)-1 && prices[i] >= prices[i+1] {
+			i++
+		}
+		valley = prices[i]
+		for i < len(prices)-1 && prices[i] <= prices[i+1] {
+			i++
+		}
+		peak = prices[i]
+		profit = profit + peak - valley
+	}
+	return profit
 }
