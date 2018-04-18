4c8498fc00223b245a16127cceb270f2841e1200
Julineo <advoretskiy80@gmail.com>
2018-04-18 15:03:17 -0700

    solved

--- CountAndSay/main.go
+++ CountAndSay/main.go
@@ -6,38 +6,27 @@ import (
 )
 
 func main() {
-	fmt.Printf("%s\n", countAndSay(5))
+	fmt.Printf("%s\n", countAndSay(7))
 }
 
 func countAndSay(n int) string {
-	ns := []string{"0","1"}
-    letter, letterP := "", ""
-	for i := 1; i < n; i++ {
-
-		count := 1
-        res := ""
-		for j, a := range ns[i] {
-			letter = string(a)
-            fmt.Println("j",j)
-            fmt.Println("string(a):", string(a))
-			if letterP == letter {
-				count++
-                fmt.Println("count:", count)
-			} 
-            if letterP != letter || j == len(ns[i])-1{
-                fmt.Println("second:", res, strconv.Itoa(count), letter)
-				res = res + strconv.Itoa(count) + letter
-			}
-            letterP = letter
-            fmt.Println("boom")
-		}
-        fmt.Println("letterP:", letterP)
-        fmt.Println("letter:", letter)
-        fmt.Println("res:", res)
-        ns = append(ns, res)
+	if n == 0 {
+		return ""
 	}
-    fmt.Println("ns",ns)
-	return ns[n]
+	res := "1"
+	for n := n - 1; n > 0; n-- {
+		cur := ""
+		for i := 0; i < len(res); i++ {
+			count := 1
+			for ; i + 1 < len(res) && res[i] == res[i+1]; {
+				count++
+				i++
+			}
+			cur = cur + strconv.Itoa(count) + string(res[i])
+		}
+	res = cur
+	}
+	return res
 }
 
 func countAndSayReversed(n int) string {
