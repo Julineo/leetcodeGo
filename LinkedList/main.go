e3f535a8a770e21d0fda6bd09a7fa6ed2d24e644
Julineo <advoretskiy80@gmail.com>
2018-04-24 16:47:58 -0700

    worked

--- LinkedList/main.go
+++ LinkedList/main.go
@@ -10,23 +10,45 @@ type ListNode struct {
 }
 
 func main() {
-	//input := []int{9,4,3,4,5,7,4,2}
-	input := []int{10,9,8,8,9,10}
+	input := []int{9,4,3,4,5,7,4,2}
+	input = []int{10,9,8,8,9,10}
 	input = []int{10,9,8,9,10}
 
 	//creating linked list
 	ListNodes := make([]ListNode,len(input))
-	for i := 0; i < len(input) - 1; i++ {
+	for i := 0; i < len(input); i++ {
+		fmt.Println(input[i])
 		ListNodes[i].Val = input[i]
-		ListNodes[i].Next = &ListNodes[i+1]
+		if i < len(input) - 1 {
+			ListNodes[i].Next = &ListNodes[i+1]
+		}
 	}
 
-	isPalindrome(&ListNodes[0])
-	isPalindrome(&ListNodes[1])
-	isPalindrome(&ListNodes[2])
+
+	fmt.Println(isPalindrome(&ListNodes[0]))
+
 }
 
 func isPalindrome(head *ListNode) bool {
-	fmt.Println(head)
-	return false
+    slow, fast := head, head
+    var prev *ListNode
+
+    for fast != nil && fast.Next != nil {
+        fast = fast.Next.Next
+        next := slow.Next
+        slow.Next = prev
+        prev = slow
+        slow = next
+    }
+
+    first, second := prev, slow
+    if fast != nil { second = second.Next }
+
+    for second != nil {
+        if first.Val != second.Val {
+            return false
+        }
+        first, second = first.Next, second.Next
+    }
+    return true
 }
