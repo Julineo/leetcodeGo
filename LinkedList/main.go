daec2e14b2c126d085fcc194054a551857d28312
Julineo <advoretskiy80@gmail.com>
2018-04-25 15:44:52 -0700

    solved

--- LinkedList/main.go
+++ LinkedList/main.go
@@ -5,21 +5,22 @@ import (
 )
 
 type ListNode struct {
-	Val int
+	Val  int
 	Next *ListNode
 }
 
 func main() {
-	input := []int{9,4,3,4,5,7,4,2}
-	input = []int{10,9,8,8,9,10}
-	//input = []int{10,9,8,9,10}
+	input := []int{9, 4, 3, 4, 5, 7, 4, 2}
+	input = []int{10, 9, 8, 8, 9, 10}
+	input = []int{10,9,7,8,7,9,10}
+	input = []int{1,2}
 
 	//creating linked list
-	ListNodes := make([]ListNode,len(input))
+	ListNodes := make([]ListNode, len(input))
 	for i := 0; i < len(input); i++ {
 		fmt.Println(input[i])
 		ListNodes[i].Val = input[i]
-		if i < len(input) - 1 {
+		if i < len(input)-1 {
 			ListNodes[i].Next = &ListNodes[i+1]
 		}
 		fmt.Printf("node: %p %v\n", &ListNodes[i], ListNodes[i])
@@ -31,11 +32,16 @@ func main() {
 
 }
 
+//checks if linked list is palindrome
 func isPalindrome(head *ListNode) bool {
+	if head == nil || head.Next == nil {
+		return true
+	}
+	//reverse first half of a list
 	slow, fast := head, head
 	var prev, next *ListNode
 
-for fast != nil && fast.Next != nil {
+	for fast != nil && fast.Next != nil {
 		fmt.Println("loop")
 		fast = fast.Next.Next
 		next = slow.Next
@@ -47,6 +53,26 @@ for fast != nil && fast.Next != nil {
 		fmt.Printf("slow: %p %v\n", slow, slow)
 		fmt.Printf("prev: %p %v\n", prev, prev)
 	}
-	return false
-}
 
+	//getting ready for camperison, assuming odd and even cases
+	var first, second *ListNode
+	if fast == nil {
+		first = prev
+		second = next
+	} else {
+		first = prev
+		second = next.Next
+	}
+
+	//compare two parts of a list
+	for first != nil {
+		fmt.Printf("first: %p %v\n", first, first)
+		fmt.Printf("second: %p %v\n", second, second)
+		if first.Val != second.Val {
+			return false
+		}
+		first = first.Next
+		second = second.Next
+	}
+	return true
+}
