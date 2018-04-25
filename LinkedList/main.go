83ead6e39a02c0310f92cb5bfa319e92b00c9bcc
Julineo <advoretskiy80@gmail.com>
2018-04-25 12:43:36 -0700

    worked

--- LinkedList/main.go
+++ LinkedList/main.go
@@ -12,7 +12,7 @@ type ListNode struct {
 func main() {
 	input := []int{9,4,3,4,5,7,4,2}
 	input = []int{10,9,8,8,9,10}
-	input = []int{10,9,8,9,10}
+	//input = []int{10,9,8,9,10}
 
 	//creating linked list
 	ListNodes := make([]ListNode,len(input))
@@ -22,33 +22,31 @@ func main() {
 		if i < len(input) - 1 {
 			ListNodes[i].Next = &ListNodes[i+1]
 		}
+		fmt.Printf("node: %p %v\n", &ListNodes[i], ListNodes[i])
 	}
 
-
+	fmt.Println(ListNodes)
 	fmt.Println(isPalindrome(&ListNodes[0]))
+	fmt.Println(ListNodes)
 
 }
 
 func isPalindrome(head *ListNode) bool {
-    slow, fast := head, head
-    var prev *ListNode
+	slow, fast := head, head
+	var prev, next *ListNode
 
-    for fast != nil && fast.Next != nil {
-        fast = fast.Next.Next
-        next := slow.Next
-        slow.Next = prev
-        prev = slow
-        slow = next
-    }
-
-    first, second := prev, slow
-    if fast != nil { second = second.Next }
-
-    for second != nil {
-        if first.Val != second.Val {
-            return false
-        }
-        first, second = first.Next, second.Next
-    }
-    return true
+for fast != nil && fast.Next != nil {
+		fmt.Println("loop")
+		fast = fast.Next.Next
+		next = slow.Next
+		slow.Next = prev
+		prev = slow
+		slow = next
+		fmt.Printf("fast: %p %v\n", fast, fast)
+		fmt.Printf("next: %p %v\n", next, next)
+		fmt.Printf("slow: %p %v\n", slow, slow)
+		fmt.Printf("prev: %p %v\n", prev, prev)
+	}
+	return false
 }
+
