e8708dc1c4187d856f4d90b96e8b096a9cfd8ad1
Julineo <advoretskiy80@gmail.com>
2018-07-12 16:34:41 -0700

    solved

--- LinkedList/RemoveNthNodeFromEndofList/main.go
+++ LinkedList/RemoveNthNodeFromEndofList/main.go
@@ -1,3 +1,21 @@
+/*
+https://leetcode.com/explore/learn/card/linked-list/214/two-pointer-technique/1296/
+
+Given a linked list, remove the n-th node from the end of list and return its head.
+
+Example:
+
+Given linked list: 1->2->3->4->5, and n = 2.
+
+After removing the second node from the end, the linked list becomes 1->2->3->5.
+Note:
+
+Given n will always be valid.
+
+Follow up:
+
+Could you do this in one pass?
+*/
 package main
 
 import "fmt"
@@ -8,15 +26,19 @@ import "fmt"
  }
 
 func main() {
-	l := ListNode{}
+	var l *ListNode
+	l = &ListNode{}
 	l.Val = 1
-	fmt.Println(l, &l)
 	for i := 2; i <= 5; i++{
 		l.AddAtTail(i)
 	}
 	l.showList()
+	l = removeNthFromEnd(l, 5)
+	fmt.Println("end")
+	l.showList()
 }
 
+// prints tes linked list
 func (this *ListNode) showList() {
 	var cur *ListNode
 	cur = this
@@ -26,10 +48,32 @@ func (this *ListNode) showList() {
 	}
 }
 
+// removes nodes
 func removeNthFromEnd(head *ListNode, n int) *ListNode {
-	return &ListNode{}
+	var slow, fast, prev *ListNode
+	slow, fast = head, head
+	// move fast n-times foreward
+	for i := 1; i < n; i++ {
+		fast = fast.Next
+	}
+	// move fast and slow until the end of list
+	for {
+		if fast.Next == nil {
+			break
+		}
+		prev = slow
+		fast = fast.Next
+		slow = slow.Next
+		fmt.Println(fast, slow, prev)
+	}
+	if prev == nil {
+		return slow.Next
+	}
+	prev.Next = slow.Next
+	return head
 }
 
+// adds nodes at tail
 func (this *ListNode) AddAtTail(val int)  {
 	var newTail, cur *ListNode
 	newTail = &ListNode{val, nil}
