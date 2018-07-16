9583b62c432aaa78eedd3421c99ebaba67b977df
Julineo <advoretskiy80@gmail.com>
2018-07-16 11:37:36 -0700

    worked on Transpose Matrix

--- TransposeMatrix/main.go
+++ TransposeMatrix/main.go
@@ -1,6 +1,15 @@
 package main
 
+import "fmt"
+
 func transpose(A [][]int) [][]int {
-	
+	m := len(A)
+	n := len(A[0])
+	B := make([][]int, n)
+	for i := 0; i < n; i++ {
+		B[i] = make([]int, m)
+	}
+	fmt.Println(B)
+	return B
 }
 
--- TransposeMatrix/main_test.go
+++ TransposeMatrix/main_test.go
@@ -1,19 +1,20 @@
 package main
 
 import (
-	"testing",
-	
+	"testing"
+	"reflect"
 )
 
 func TestTranspose(t *testing.T) {
 	var tests = []struct {
-		[][]A int
-		[][]want int
+		A[][]int
+		want[][]int
 	}{
 		{[][]int{{1,2,3},{4,5,6},{7,8,9}}, [][]int{{1,4,7},{2,5,8},{3,6,9}}},
-		{[][]int{{1,2,3},{4,5,6}}, [][]int{{1,4},{2,5},{3,6}}}
+		{[][]int{{1,2,3},{4,5,6}}, [][]int{{1,4},{2,5},{3,6}}},
+	}
 	for _, test := range tests {
-		if got := transpose(test.A); got != test.want {
+		if got := transpose(test.A); ! reflect.DeepEqual(got, test.want) {
 			t.Errorf("transpose(%v) = %v", test.A, got)
 		}
 	}
