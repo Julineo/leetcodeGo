7aa76907b973e4a3168110941fdae36788fc71ac
Julineo <advoretskiy80@gmail.com>
2018-07-17 14:17:51 -0700

    solved

--- TransposeMatrix/main.go
+++ TransposeMatrix/main.go
@@ -1,3 +1,21 @@
+/*
+https://leetcode.com/problems/transpose-matrix/description/
+Given a matrix A, return the transpose of A.
+
+The transpose of a matrix is the matrix flipped over it's main diagonal, switching the row and column indices of the matrix.
+
+ 
+
+Example 1:
+
+Input: [[1,2,3],[4,5,6],[7,8,9]]
+Output: [[1,4,7],[2,5,8],[3,6,9]]
+Example 2:
+
+Input: [[1,2,3],[4,5,6]]
+Output: [[1,4],[2,5],[3,6]]
+*/
+
 package main
 
 import "fmt"
@@ -9,7 +27,11 @@ func transpose(A [][]int) [][]int {
 	for i := 0; i < n; i++ {
 		B[i] = make([]int, m)
 	}
-	fmt.Println(B)
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			B[j][i] = A[i][j]
+		}
+	}
 	return B
 }
 
