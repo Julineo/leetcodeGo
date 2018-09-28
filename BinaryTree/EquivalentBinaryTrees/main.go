8ea5d76b74378a961e6f5162e81a4e651230dbef
Julineo <advoretskiy80@gmail.com>
2018-09-27 17:00:58 -0700

    solved Same tree task

--- BinaryTree/EquivalentBinaryTrees/main.go
+++ BinaryTree/EquivalentBinaryTrees/main.go
@@ -29,15 +29,34 @@ func Walk(t *tree.Tree, ch chan int) {
 // Same determines whether the trees
 // t1 and t2 contain the same values.
 func Same(t1, t2 *tree.Tree) bool {
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	go Walk(t1, ch1)
+	go Walk(t2, ch2)
 
-	return false
+	for {
+		v1, ok1 := <- ch1
+		v2, ok2 := <- ch2
+		switch {
+		case ok1 != ok2:
+			return false
+		case ok1 == false:
+			return true
+		case v1 != v2:
+			return false
+		}
+
+	}
 }
 
 func main() {
 //	fmt.Println(tree.New(1))
 	ch := make(chan int)
-	go Walk(tree.New(1), ch)
+	go Walk(tree.New(2), ch)
 	for v := range ch {
 		fmt.Println(v)
 	}
+
+	fmt.Println(Same(tree.New(1), tree.New(1)))
+	fmt.Println(Same(tree.New(1), tree.New(2)))
 }
